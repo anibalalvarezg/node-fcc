e11e7b27b5da4698453a258c08b7b8f2cf835620
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-05-08 21:44:01 -0400

    Alternative exports

--- 3-modules.js
+++ 3-modules.js
@@ -3,6 +3,10 @@
 
 const { john, peter } = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./6-alternative-exports')
+
+console.log(data)
+
 sayHi('susan')
 sayHi(john)
 sayHi(peter)
--- 4-names.js
+++ 4-names.js
@@ -6,12 +6,3 @@ const john = 'john'
 const peter = 'peter'
 
 module.exports = { john, peter }
-
-
-module.exports.items = ['item1', 'item2']
-
-const person = { 
-    name: 'bob'
-}
-
-module.exports.singlePerson = person
